e783942aaf7302135e37ab842ffa7a4456c81962
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-11-15 09:06:35 +0530

    program added

---
+++ Day22_IndianStateCensusAnalyzer/CSVAdapterFactory.cs
@@ -0,0 +1,21 @@
+﻿using Day22_IndianStateCensusAnalyzer.DTO;
+using System.Collections.Generic;
+
+namespace Day22_IndianStateCensusAnalyzer
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
